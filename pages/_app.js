9ccd919498a1d0a8a4666eda148915837f2c5331
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-07-24 15:38:55 +0430

    practicing router in next

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import "../styles/globals.css";
 import Navbar from "../component/navbar";
-
+import Footer from "../component/footer";
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps}></Component>
+      <Footer />
     </>
   );
 }
